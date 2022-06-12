3f5f15ba51fa43315804ccaafcfa0a465f59f6f0
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-06-12 12:09:26 -0700

    added all the map files

---
+++ mapdata.js
@@ -0,0 +1,770 @@
+var simplemaps_namap_mapdata={
+  main_settings: {
+    width: "600", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 14,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Testers In This State",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    border_hover_color: "#3af8fa"
+  },
+  state_specific: {
+    AB: {
+      name: "Alberta",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/alberta"
+    },
+    AK: {
+      name: "Alaska",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/alaska"
+    },
+    AL: {
+      name: "Alabama",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/alabama"
+    },
+    AR: {
+      name: "Arkansas",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/arkansas"
+    },
+    AZ: {
+      name: "Arizona",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/arizona"
+    },
+    BC: {
+      name: "British Columbia",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/british-columbia"
+    },
+    CA: {
+      name: "California",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/california"
+    },
+    CO: {
+      name: "Colorado",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/colorado"
+    },
+    CT: {
+      name: "Connecticut",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/connecticut"
+    },
+    DC: {
+      name: "District of Columbia",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/district-of-columbia"
+    },
+    DE: {
+      name: "Delaware",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/delaware"
+    },
+    FL: {
+      name: "Florida",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/florida"
+    },
+    GA: {
+      name: "Georgia",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/georgia"
+    },
+    HI: {
+      name: "Hawaii",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/hawaii"
+    },
+    IA: {
+      name: "Iowa",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/iowa"
+    },
+    ID: {
+      name: "Idaho",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/idaho"
+    },
+    IL: {
+      name: "Illinois",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/illinois"
+    },
+    IN: {
+      name: "Indiana",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/indiana"
+    },
+    KS: {
+      name: "Kansas",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/kansas"
+    },
+    KY: {
+      name: "Kentucky",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/kentucky"
+    },
+    LA: {
+      name: "Louisiana",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/louisiana"
+    },
+    MA: {
+      name: "Massachusetts",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/massachusetts"
+    },
+    MB: {
+      name: "Manitoba",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/manitoba"
+    },
+    MD: {
+      name: "Maryland",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/maryland"
+    },
+    ME: {
+      name: "Maine",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/maine"
+    },
+    MI: {
+      name: "Michigan",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/michigan"
+    },
+    MN: {
+      name: "Minnesota",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/minnesota"
+    },
+    MO: {
+      name: "Missouri",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/missouri"
+    },
+    MS: {
+      name: "Mississippi",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/mississippi"
+    },
+    MT: {
+      name: "Montana",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/montana"
+    },
+    MX: {
+      name: "Mexico",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/mexico"
+    },
+    NB: {
+      name: "New Brunswick",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/new-brunswick"
+    },
+    NC: {
+      name: "North Carolina",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/north-carolina"
+    },
+    ND: {
+      name: "North Dakota",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/north-dakota"
+    },
+    NE: {
+      name: "Nebraska",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/nebraska"
+    },
+    NH: {
+      name: "New Hampshire",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/new-hampshire"
+    },
+    NJ: {
+      name: "New Jersey",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/new-jersey"
+    },
+    NL: {
+      name: "Newfoundland and Labrador",
+      description: "Click here to see testers in this location",
+      url: "https://www.fumehoodtestingnetwork.com/testers/newfoundland-and-labrador"
+    },
+    NM: {
+      name: "New Mexico",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/new-mexico"
+    },
+    NS: {
+      name: "Nova Scotia",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/nova-scotia"
+    },
+    NT: {
+      name: "Northwest Territories",
+      description: "Click here to see testers in this location",
+      hide: "yes"
+    },
+    NU: {
+      name: "Nunavut",
+      description: "Click here to see testers in this location",
+      hide: "yes"
+    },
+    NV: {
+      name: "Nevada",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/nevada"
+    },
+    NY: {
+      name: "New York",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/new-york"
+    },
+    OH: {
+      name: "Ohio",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/ohio"
+    },
+    OK: {
+      name: "Oklahoma",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/oklahoma"
+    },
+    ON: {
+      name: "Ontario",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/ontario"
+    },
+    OR: {
+      name: "Oregon",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/oregon"
+    },
+    PA: {
+      name: "Pennsylvania",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/pennsylvania"
+    },
+    PE: {
+      name: "Prince Edward Island",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/prince-edward-island"
+    },
+    QC: {
+      name: "Québec",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/quebec"
+    },
+    RI: {
+      name: "Rhode Island",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/rhode-island"
+    },
+    SC: {
+      name: "South Carolina",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/south-carolina"
+    },
+    SD: {
+      name: "South Dakota",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/south-dakota"
+    },
+    SK: {
+      name: "Saskatchewan",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/saskatchewan"
+    },
+    TN: {
+      name: "Tennessee",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/tennessee"
+    },
+    TX: {
+      name: "Texas",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/texas"
+    },
+    UT: {
+      name: "Utah",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/utah"
+    },
+    VA: {
+      name: "Virginia",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/virginia"
+    },
+    VT: {
+      name: "Vermont",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/vermont"
+    },
+    WA: {
+      name: "Washington",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/washington"
+    },
+    WI: {
+      name: "Wisconsin",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/wisconsin"
+    },
+    WV: {
+      name: "West Virginia",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/west-virginia"
+    },
+    WY: {
+      name: "Wyoming",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/wyoming"
+    },
+    YT: {
+      name: "Yukon",
+      description: "Click here to see testers in this location",
+      url: "https://fumehoodtestingnetwork.com/testers/yukon"
+    }
+  },
+  locations: {},
+  labels: {
+    NH: {
+      parent_id: "NH",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 565
+    },
+    MA: {
+      parent_id: "MA",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 590
+    },
+    CT: {
+      parent_id: "CT",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 615
+    },
+    RI: {
+      parent_id: "RI",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 640
+    },
+    MD: {
+      parent_id: "MD",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 665
+    },
+    VT: {
+      parent_id: "VT",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 590
+    },
+    NJ: {
+      parent_id: "NJ",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 615
+    },
+    DE: {
+      parent_id: "DE",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 640
+    },
+    DC: {
+      parent_id: "DC",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 665
+    },
+    HI: {
+      parent_id: "HI",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 290,
+      y: 640
+    },
+    WV: {
+      parent_id: "WV",
+      x: "803.4",
+      y: "601.7",
+      size: 11
+    },
+    TX: {
+      parent_id: "TX",
+      x: 630,
+      y: 695
+    },
+    AK: {
+      parent_id: "AK",
+      x: 375,
+      y: 201
+    },
+    PE: {
+      parent_id: "PE",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 965,
+      y: 490
+    },
+    NS: {
+      parent_id: "NS",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 965,
+      y: 515
+    },
+    MX: {
+      parent_id: "MX",
+      x: "601.3",
+      y: "788.5",
+      name: "Mexico"
+    },
+    MB: {
+      parent_id: "MB",
+      x: "656.4",
+      y: "417.8"
+    },
+    SK: {
+      parent_id: "SK",
+      x: "595.6",
+      y: "425.6"
+    },
+    AB: {
+      parent_id: "AB",
+      x: "534.6",
+      y: "407"
+    },
+    BC: {
+      parent_id: "BC",
+      x: "470.8",
+      y: "383.5"
+    },
+    NU: {
+      parent_id: "NU",
+      x: "652",
+      y: "289.5"
+    },
+    NT: {
+      parent_id: "NT",
+      x: "528.4",
+      y: "293.2"
+    },
+    YT: {
+      parent_id: "YT",
+      x: "438.5",
+      y: "275.6"
+    },
+    ON: {
+      parent_id: "ON",
+      x: "729.8",
+      y: "452.6"
+    },
+    QC: {
+      parent_id: "QC",
+      x: "833.9",
+      y: "431.3"
+    },
+    NB: {
+      parent_id: "NB",
+      x: "900.6",
+      y: "483.3"
+    },
+    NL: {
+      parent_id: "NL",
+      x: "901.1",
+      y: "384.5"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "677.5",
+      y: "520.6"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "557.2",
+      y: "507.5"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "630.3",
+      y: "509.3"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "509.2",
+      y: "540.2"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "476.8",
+      y: "485"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "516.2",
+      y: "650.8"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "447.7",
+      y: "608.3"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "579.7",
+      y: "604.6"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "481.5",
+      y: "582.4"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "569.4",
+      y: "656.2"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "462",
+      y: "524.9"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "526.1",
+      y: "595.7"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "567.6",
+      y: "556.2"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "699.3",
+      y: "655.2"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "687.5",
+      y: "573.4"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "644.2",
+      y: "614.6"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "697.5",
+      y: "617.3"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "632.8",
+      y: "579.3"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "656.4",
+      y: "650.3"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "630.6",
+      y: "544.5"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "699.3",
+      y: "691"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "756.2",
+      y: "677.6"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "813.1",
+      y: "719.2"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "790.9",
+      y: "675.4"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "727.7",
+      y: "679.7"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "813.1",
+      y: "657"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "724.8",
+      y: "593"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "753.5",
+      y: "594.2"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "772.2",
+      y: "617"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "831.6",
+      y: "634.4"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "781",
+      y: "587.7"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "756.7",
+      y: "641.3"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "822.2",
+      y: "611.8"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "715.9",
+      y: "539.3"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "844.7",
+      y: "541.3"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "824.8",
+      y: "571.4"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "882.7",
+      y: "502.6"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "760.2",
+      y: "556.5"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
